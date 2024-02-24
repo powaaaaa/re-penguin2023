31c891a697e9f883b96903d5f44ee2ea7496b4d6
powaaaaa <knyuki2525@gmail.com>
2024-02-24 22:45:09 +0900

    feat: component Button

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,46 @@
+import { Button } from "./Button.tsx";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  render: (args) => <Button {...args}>{args.children}</Button>,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: "button",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    children: "button",
+    variant: "outlined",
+    size: "large",
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    children: "button",
+    variant: "outlined",
+    size: "medium",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    children: "button",
+    variant: "contained",
+    size: "small",
+  },
+};
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,34 @@
+import { cva } from "class-variance-authority";
+import { ComponentPropsWithoutRef, FC, ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+    variant?: "contained" | "outlined";
+    size?: "large" | "medium" | "small";
+} & ComponentPropsWithoutRef<"button">
+
+export const Button: FC<Props> = ({children, variant, size, ...props}) => {
+    const style = cva(`rounded ${props.className}`, {
+        variants: {
+            variant: {
+                contained: "bg-[#8FD0D0]",
+                outlined: "border-[#aaddcc]",
+            },
+            size: {
+                large: "py-3 px-8 border-4 text-xl font-bold",
+                medium: "py-1 px-6 border-2 text-base font-medium",
+                small: "py-1 px-3 text-xs font-medium",
+            }
+        },
+        defaultVariants: {
+            variant: "outlined",
+            size: "medium",
+        }
+    });
+
+    return (
+        <button className={style({ variant, size })} {...props} >
+            {children}
+        </button>
+    )
+}
