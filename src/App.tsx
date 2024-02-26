13537063cb2fb37ca097cbb9abac2078b56bf129
powaaaaa <knyuki2525@gmail.com>
2024-02-26 19:50:47 +0900

    feat: favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { HomePage } from "./components/pages/home";
-import { Helmet } from "react-helmet-async";
 
 function App() {
   // return <>Re: penguin hack 2023</>;
   return (
     <>
-      <Helmet>
-        <title>TODOのなかまたち</title>
-      </Helmet>
       <HomePage />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <title>TODOのなかまたち</title>
+        <link rel="icon" type="image/png" href="./favicon.png"></link>
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
-)
+);
