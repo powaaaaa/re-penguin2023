1b5d25f20e92c638658fe390e30d7582f51048e1
powaaaaa <knyuki2525@gmail.com>
2024-02-26 19:33:34 +0900

    feat: Head title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { HomePage } from "./components/pages/home";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   // return <>Re: penguin hack 2023</>;
-  return <HomePage />;
+  return (
+    <>
+      <Helmet>
+        <title>TODOのなかまたち</title>
+      </Helmet>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
