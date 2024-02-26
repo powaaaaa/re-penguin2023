5288ff3df643b7e65e7b161a61b865ccd6d135f8
powaaaaa <knyuki2525@gmail.com>
2024-02-26 17:26:10 +0900

    update: images name

--- src/components/features/Asika/Asika.tsx
+++ src/components/features/Asika/Asika.tsx
@@ -7,14 +7,14 @@ type Props = {
 };
 
 const imageMappings: ImageMappingType[] = [
-  { src: "/images/zp1.png", min: 0, max: 2 },
-  { src: "/images/kp1.png", min: 3, max: 6 },
-  { src: "/images/km1.png", min: 7, max: 9 },
-  { src: "/images/ap1.png", min: 10, max: 14 },
-  { src: "/images/default.png", min: 15, max: 19 },
-  { src: "/images/am1.png", min: 20, max: 29 },
-  { src: "/images/zm1.png", min: 30, max: 99 },
-  { src: "/images/over.png", min: 100, max: Infinity },
+  { src: "/images/1.png", min: 0, max: 2 },
+  { src: "/images/2.png", min: 3, max: 6 },
+  { src: "/images/3.png", min: 7, max: 9 },
+  { src: "/images/4.png", min: 10, max: 14 },
+  { src: "/images/5.png", min: 15, max: 19 },
+  { src: "/images/6.png", min: 20, max: 29 },
+  { src: "/images/7.png", min: 30, max: 99 },
+  { src: "/images/8.png", min: 100, max: Infinity },
 ];
 
 export const Asika: FC<Props> = ({ status }) => {
